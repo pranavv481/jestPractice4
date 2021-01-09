8987e78875f36bf2e3bd8c3347ea718647926803
pranavv481 <pranavv481@gmail.com>
2021-01-09 20:12:48 +0530

    redux store

--- test/testUtil.js
+++ test/testUtil.js
@@ -1,12 +1,17 @@
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
+import rootReducer from "../src/reducers/index";
 
+export const storeFactory = (initialState) => {
+    return createStore(rootReducer, initialState)
+}
 
-export const findByTestAttr = (wrapper, val) =>{
+export const findByTestAttr = (wrapper, val) => {
     return wrapper.find(`[data-test="${val}"]`)
-} 
+}
 
 
-export const checkProps = (component, conformingProps)=>{
-   const propError = checkPropTypes(component.propTypes,conformingProps,'prop',component.name)
-   expect(propError).toBeUndefined();
+export const checkProps = (component, conformingProps) => {
+    const propError = checkPropTypes(component.propTypes, conformingProps, 'prop', component.name)
+    expect(propError).toBeUndefined();
 } 
